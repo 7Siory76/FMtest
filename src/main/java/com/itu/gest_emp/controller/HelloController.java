0f17197b4aec82b4d6133b6b1b08775239b09860
7Siory76 <tsioryrandroso7@gmail.com>
2026-03-07 14:01:57 +0300

    sprint4

--- src/main/java/com/itu/gest_emp/controller/HelloController.java
+++ src/main/java/com/itu/gest_emp/controller/HelloController.java
@@ -6,15 +6,33 @@
  @Controller
  public class HelloController{
 
+     // Test 1 : retour String -> affiche directement
      @Url("/hello")
      public String hello() {
          return "Bonjour depuis le framework !";
      }
 
+     // Test 2 : retour String -> affiche directement
      @Url("/bye")
      public String bye() {
          return "Au revoir !";
      }
 
+     // Test 3 : retour .jsp -> forward vers la JSP
+     @Url("/page")
+     public String page() {
+         return "/pages/hello.jsp";
+     }
+
+     @Url("/bug")
+     public int bug() {
+         return 42;
+     }
+
+     // Test 4 : retour int (non supporte) -> message d'erreur
+     @Url("/error")
+     public int error() {
+         return 42;
+     }
 
  }
